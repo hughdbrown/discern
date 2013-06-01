66981c6e12b9061f0583b1f960e0daca9853bafb
Hugh Brown <hbrown@wgen.net>
2013-05-31 20:40:35 -0400

    ./docs/examples/common_settings.py: E261 whitespace after inline comment

--- docs/examples/common_settings.py
+++ docs/examples/common_settings.py
@@ -2,17 +2,17 @@
 Define some constants and imports that we will use for all the examples.
 """
 
-#Common imports.
-#Requests allows us to make http GET/POST/PUT, etc requests.
-#See http://docs.python-requests.org/en/latest/ for documentation
+# Common imports.
+# Requests allows us to make http GET/POST/PUT, etc requests.
+# See http://docs.python-requests.org/en/latest/ for documentation
 import requests
-#JSON is a format for transferring data over the web.  This imports a json handling library.
-#See http://en.wikipedia.org/wiki/JSON for information on JSON.
+# JSON is a format for transferring data over the web.  This imports a json handling library.
+# See http://en.wikipedia.org/wiki/JSON for information on JSON.
 import json
 
 from pprint import *
 
-#This tells us where the API is running.
+# This tells us where the API is running.
 API_BASE_URL = "http://127.0.0.1:7999"
 
 headers = {'content-type': 'application/json'}
