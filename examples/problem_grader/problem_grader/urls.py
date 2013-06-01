39faa445147510e0b73cc4bd0fdcb8af5197eb5b
Hugh Brown <hbrown@wgen.net>
2013-05-31 20:42:04 -0400

    ./examples/problem_grader/problem_grader/urls.py: E203 whitespace before colon

--- examples/problem_grader/problem_grader/urls.py
+++ examples/problem_grader/problem_grader/urls.py
@@ -17,6 +17,6 @@ if settings.DEBUG:
     urlpatterns += patterns('',
                            url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
                                'document_root': settings.STATIC_ROOT,
-                               'show_indexes' : True,
+                               'show_indexes': True,
                                }),
                            )
