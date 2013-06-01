6a39538e2c1f3942eabd5084e39240329520d555
Hugh Brown <hbrown@wgen.net>
2013-05-31 20:41:02 -0400

    ./freeform_data/django_validators.py: E225 space around operator

--- freeform_data/django_validators.py
+++ freeform_data/django_validators.py
@@ -57,18 +57,18 @@ class JSONListValidator(object):
         # Each value must be an integer
         for val in value:
             if not isinstance(val,int):
-                error_message="You entered a non-integer value in your score list. {0}".format(value)
+                error_message = "You entered a non-integer value in your score list. {0}".format(value)
                 raise ValidationError(error_message)
 
         # Validate the lengths to ensure they match
         if self.matching_list_len is not None:
-            if self.matching_list_len!=value_len:
+            if self.matching_list_len != value_len:
                 error_message = "You entered more target scores than exist in the corresponding maximum list in the problem.  {0} vs {1}".format(value_len, self.matching_list_len)
                 raise ValidationError(error_message)
 
         # Validate each value to make sure it is less than the max allowed.
         if self.matching_list is not None:
             for i in xrange(0,self.matching_list_len):
-                if value[i]>self.matching_list[i]:
+                if value[i] > self.matching_list[i]:
                     error_message = "Value {i} in provided scores greater than max defined in problem. {value} : {matching}".format(i=i, value=value, matching=self.matching_list)
                     raise ValidationError(error_message)
