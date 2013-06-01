e02f7e42e17158e7317ad81c2f6143c56930e38b
Hugh Brown <hbrown@wgen.net>
2013-05-31 20:47:33 -0400

    ./examples/problem_grader/problem_grader/urls.py: E128 continuation line under-indented for visual indent

--- examples/problem_grader/problem_grader/urls.py
+++ examples/problem_grader/problem_grader/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
 if settings.DEBUG:
     # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, show_indexes=True)
     urlpatterns += patterns('',
-                           url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+                            url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
                                'document_root': settings.STATIC_ROOT,
                                'show_indexes': True,
                                }),
