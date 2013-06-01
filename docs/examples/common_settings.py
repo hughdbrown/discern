c83b6a2d61c5b4236d27f1eae113f9b44d7bade1
Hugh Brown <hbrown@wgen.net>
2013-05-31 20:37:30 -0400

    ./docs/examples/common_settings.py: W291 trailing whitespace

--- docs/examples/common_settings.py
+++ docs/examples/common_settings.py
@@ -17,11 +17,11 @@ API_BASE_URL = "http://127.0.0.1:7999"
 
 headers = {'content-type': 'application/json'}
 
-# Most of the scripts will need to login, use this function to avoid repeating code. 
+# Most of the scripts will need to login, use this function to avoid repeating code.
 def login_to_discern(session, username='test', password='test'):
 	login_url = API_BASE_URL + "/essay_site/login/"
 	return session.post(
-			login_url, 
+			login_url,
 			json.dumps({
 				'username' : username,
 				'password' : password,}),
