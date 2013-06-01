c22d9a316be7f57096de63db18067461923c906a
Hugh Brown <hbrown@wgen.net>
2013-05-31 20:41:42 -0400

    ./discern/aws.py: E231 missing whitespace after ','

--- discern/aws.py
+++ discern/aws.py
@@ -14,7 +14,7 @@ THROTTLE_AT = 100  # Throttle requests after this number in below timeframe
 THROTTLE_TIMEFRAME = 60 * 60  # Timeframe in which to throttle N requests, seconds
 THROTTLE_EXPIRATION = 24 * 60 * 60  # When to remove throttle entries from cache, seconds
 
-with open(os.path.join(ENV_ROOT,"env.json")) as env_file:
+with open(os.path.join(ENV_ROOT, "env.json")) as env_file:
     ENV_TOKENS = json.load(env_file)
 
 with open(os.path.join(ENV_ROOT, "auth.json")) as auth_file:
