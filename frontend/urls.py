385116cbe3cec36039c7d993edfa0484ffe3e97f
Hugh Brown <hbrown@wgen.net>
2013-05-31 20:47:26 -0400

    ./frontend/urls.py: E128 continuation line under-indented for visual indent

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import *
 
 urlpatterns = patterns('frontend.views',
-    url(r'^course/$', 'course'),
-    url(r'^user/$', 'user'),
-    url(r'^problem/$', 'problem'),
-    url(r'^essay/$', 'essay'),
-    url(r'^essaygrade/$', 'essaygrade'),
-    url(r'^membership/$', 'membership'),
-    url(r'^userprofile/$', 'userprofile'),
-    url(r'^organization/$', 'organization'),
-    url(r'^$', 'index'),
+                       url(r'^course/$', 'course'),
+                       url(r'^user/$', 'user'),
+                       url(r'^problem/$', 'problem'),
+                       url(r'^essay/$', 'essay'),
+                       url(r'^essaygrade/$', 'essaygrade'),
+                       url(r'^membership/$', 'membership'),
+                       url(r'^userprofile/$', 'userprofile'),
+                       url(r'^organization/$', 'organization'),
+                       url(r'^$', 'index'),
 )
