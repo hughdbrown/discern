29681099d652297cc6880d6d4a1fcf296766e1a1
Hugh Brown <hbrown@wgen.net>
2013-05-31 20:38:11 -0400

    ./docs/examples/delete_objects.py: W191 indentation contains tabs

--- docs/examples/delete_objects.py
+++ docs/examples/delete_objects.py
@@ -16,17 +16,17 @@ problem_response = session.get(API_BASE_URL + "/essay_site/api/v1/problem/?offse
         headers=headers)
 
 for p in json.loads(problem_response.text)['objects']:
-	print (u"Problem problem: {0}, URI: {1} ".format(p['prompt'], p['resource_uri']))
-	session.delete(API_BASE_URL + p['resource_uri'] + "?format=json")
+    print (u"Problem problem: {0}, URI: {1} ".format(p['prompt'], p['resource_uri']))
+    session.delete(API_BASE_URL + p['resource_uri'] + "?format=json")
 
 # problem_response = session.delete(API_BASE_URL + "/essay_site/api/v1/problem/1/?format=json",
 #  	headers=headers)
 
 # courses
 course_response = session.delete(API_BASE_URL + "/essay_site/api/v1/course/1/?format=json",
- 	headers=headers)
+        headers=headers)
 
 
 # organizations.
 org_response = session.delete(API_BASE_URL + "/essay_site/api/v1/organization/1/?format=json",
-	headers=headers)
+        headers=headers)
