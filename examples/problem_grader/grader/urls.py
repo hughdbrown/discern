4c8860d42a8be875cd83bebec3592fed95667ced
Hugh Brown <hbrown@wgen.net>
2013-05-31 20:41:21 -0400

    ./examples/problem_grader/grader/urls.py: E225 space around operator

--- examples/problem_grader/grader/urls.py
+++ examples/problem_grader/grader/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import *
 
-urlpatterns=patterns('django.contrib.auth.views',
+urlpatterns = patterns('django.contrib.auth.views',
                      url(r'^login/$','login'),
                      url(r'^logout/$','logout'),
                      )
 
-urlpatterns +=patterns('grader.views',
+urlpatterns += patterns('grader.views',
                        url(r'^register/$','register'),
                        url(r'^$','index'),
                        url(r'^course/$','course'),
