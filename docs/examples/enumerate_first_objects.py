f3c2ccc164579c7542698ef1309a8f432bee0041
Hugh Brown <hbrown@wgen.net>
2013-05-31 20:47:29 -0400

    ./docs/examples/enumerate_first_objects.py: E128 continuation line under-indented for visual indent

--- docs/examples/enumerate_first_objects.py
+++ docs/examples/enumerate_first_objects.py
@@ -12,19 +12,19 @@ response = login_to_discern(session)
 #  enumerate first 20 objects.
 
 problem_response = session.get(API_BASE_URL + "/essay_site/api/v1/problem/?offset=0&limit=20&f?format=json",
-        headers=headers)
+                               headers=headers)
 
 for p in json.loads(problem_response.text)['objects']:
     print (u"Problem problem: {0}, URI: {1} ".format(p['prompt'], p['resource_uri']))
 
 course_response = session.get(API_BASE_URL + "/essay_site/api/v1/course/?offset=0&limit=20&f?format=json",
-        headers=headers)
+                              headers=headers)
 
 for c in json.loads(course_response.text)['objects']:
     print (u"Course name: {0}, URI: {1} ".format(c['course_name'], c['resource_uri']))
 
 org_response = session.get(API_BASE_URL + "/essay_site/api/v1/organization/?offset=0&limit=20&f?format=json",
-        headers=headers)
+                           headers=headers)
 
 for org in json.loads(org_response.text)['objects']:
     print (u"Organization name: {0}, URI: {1} ".format(org['organization_name'], org['resource_uri']))
