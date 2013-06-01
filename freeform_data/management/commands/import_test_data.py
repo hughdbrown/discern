c659070bd398bd58bbd242e95137c13064713b1f
Hugh Brown <hbrown@wgen.net>
2013-05-31 20:40:24 -0400

    ./freeform_data/management/commands/import_test_data.py: E261 whitespace after inline comment

--- freeform_data/management/commands/import_test_data.py
+++ freeform_data/management/commands/import_test_data.py
@@ -2,8 +2,8 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.utils import timezone
 
-#from http://jamesmckay.net/2009/03/django-custom-managepy-commands-not-committing-transactions/
-#Fix issue where db data in manage.py commands is not refreshed at all once they start running
+# from http://jamesmckay.net/2009/03/django-custom-managepy-commands-not-committing-transactions/
+# Fix issue where db data in manage.py commands is not refreshed at all once they start running
 from django.db import transaction
 transaction.commit_unless_managed()
 
@@ -51,7 +51,7 @@ class Command(BaseCommand):
         try:
             User.objects.create_user('vik', 'vik@edx.org', 'vik')
         except:
-            #User already exists, but doesn't matter to us
+            # User already exists, but doesn't matter to us
             pass
 
         user = User.objects.get(username='vik')
