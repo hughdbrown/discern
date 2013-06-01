7456947579bcc92a1f9883e9b16f93ae438c3fd1
Hugh Brown <hbrown@wgen.net>
2013-05-31 20:39:09 -0400

    ./ml_grading/mock_ml_grading.py: E302 line spacing between functions and classes

--- ml_grading/mock_ml_grading.py
+++ ml_grading/mock_ml_grading.py
@@ -2,6 +2,7 @@ def grade(grader_data,student_response):
     result_dict = {'errors': [],'tests': [],'score': 0, 'feedback' : "", 'success' : False, 'confidence' : 1}
     return result_dict
 
+
 def create(essay_text, scores, prompt):
     result_dict = {'errors': [],'success' : False, 'cv_kappa' : 0, 'cv_mean_absolute_error': 0,
      'feature_ext' : "", 'classifier' : "", 'algorithm' : "C",
