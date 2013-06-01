67cf5da9e204068019d475cc062cbe9981ac0077
Hugh Brown <hbrown@wgen.net>
2013-05-31 20:40:37 -0400

    ./discern/urls.py: E261 whitespace after inline comment

--- discern/urls.py
+++ discern/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
 )
 
 if settings.DEBUG:
-    #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, show_indexes=True)
+    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, show_indexes=True)
     urlpatterns+= patterns('',
         url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.STATIC_ROOT,
